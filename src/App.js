0478eef5fa065852c95b20cce4c7d866eda83424
Kiran kumar <kirankumar17897@gmail.com>
2021-12-19 01:42:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import BootstrapTheam from "./components/BootstrapTheamKit";
+import "./style/app.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <BootstrapTheam />
+    </div>
+  );
+}
+
+export default App;
